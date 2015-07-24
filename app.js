0be9e32566d1fd248d9770544d66fa2d46980118
kevin jose <kevin.jose@aot-technologies.com>
2015-07-24 13:22:44 +0530

    added PUT and delete

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(express.static(__dirname + '/public'));
 
 app.use(function(req, res, next) {
 	res.setHeader('Access-Control-Allow-Origin', '*');
-	res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
 	res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Authorization');
 	next();
 });
